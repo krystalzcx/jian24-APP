c1b8845f5b2c5cfd5cdcd93c321f612694b1a8db
1615081154@qq.com <awesomex005@gmail.com>
2019-12-07 17:54:41 +0800

    get cookies from testcases/login.yml and every api can run by itself

--- debugtalk.py
+++ debugtalk.py
@@ -1,6 +1,9 @@
 import os
 import time
+import requests
+from httprunner.api import HttpRunner
 
+COOKIES_PATH = r'data/cookies'
 
 
 def sleep(n_secs):
@@ -21,4 +24,24 @@ def franchisee_name():
         var = 'not_sure'
     return var
 
+def generate_cookies():
+    if (not os.path.exists(COOKIES_PATH)) or (
+            3600 < int(time.time()) - int(os.path.getctime(COOKIES_PATH))):
+        # cookies 文件不存在 或 最后修改时间超过 3600 秒 (1小时) 则重新登录刷新 cookies
+        runner = HttpRunner()
+        runner.run(r'testcases/login.yml')
+    with open(COOKIES_PATH, 'r') as f:
+        cookies: str = f.read()
+    return cookies
 
+def teardown_saveCookies(response: requests.Response):
+    """保存 cookies 到文件给其他 api 调用"""
+    cookies: dict = response.cookies
+    foo: list = []
+    # 遍历 cookies 拆分 dict 并拼接为特定格式的 str
+    # 如: server=xxxxx; sid=xxxxxx; track=xxxxx;
+    for k, v in cookies.items():
+        foo.append(k + '=' + v + '; ')
+    bar: str = "".join(foo)
+    with open(COOKIES_PATH, 'w') as f:
+        f.write(bar)
