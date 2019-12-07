05c93967de2b7f58d389d2dc1ce361078a9a2c7b
1615081154@qq.com <awesomex005@gmail.com>
2019-12-07 16:49:43 +0800

    quote testcases.login.yml to keep Logon status in every testcase

--- debugtalk.py
+++ debugtalk.py
@@ -1,5 +1,24 @@
-
+import os
 import time
+
+
 
 def sleep(n_secs):
     time.sleep(n_secs)
+
+def is_superuser():
+    env = os.environ["USERNAME"]
+    if env == 'admin':
+        return True
+    else:
+        return False
+
+def franchisee_name():
+    var = 'superuser'
+    if is_superuser()==True:
+        var = 'superuser'
+    else:
+        var = 'not_sure'
+    return var
+
+
