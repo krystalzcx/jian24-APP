0e374aaef68bd00b962a23ebabd5446e0c9ad48e
krystalzcx <1615081154@qq.com>
2020-01-06 17:25:07 +0800

    modified start and end time when create promotion and search promtion

--- debugtalk.py
+++ debugtalk.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import time
 import requests
@@ -5,7 +6,7 @@ from httprunner.api import HttpRunner
 
 COOKIES_PATH = r'data/cookies'
 ID_of_promotion_PATH=r'data/ID'
-ID_of_promotion=''
+#ID_of_promotion=''
 
 def sleep(n_secs):
     time.sleep(n_secs)
@@ -14,6 +15,20 @@ def sleep(n_secs):
 def name_suffix_of_time_stamp():
     name_suffix=str(int(time.time()))
     return name_suffix
+
+def get_date():
+    today=str(datetime.date.today())
+    print(today)
+    return today
+
+def get_promtion_start_time():
+    today = str(datetime.date.today())
+    start=today+' 00:00:00'
+    return start
+def get_promtion_end_time():
+    today = str(datetime.date.today())
+    end=today+' 23:59:59'
+    return end
 
 #禁用之前的冲突的单品促销
 def disable_promtion():
@@ -24,7 +39,7 @@ def disable_promtion():
 
 
 #方式1：获取新建的状态未启用的单品促销ID_of_promotion_of_promotion
-def get_promotion_id_1(response):
+def get_first_promotion_id(response):
     id=str(response.json['info']['data'][0]['id'])
     status=response.json['info']['data'][0]['status_name']
     if status != '禁用':
@@ -33,9 +48,12 @@ def get_promotion_id_1(response):
     print(id)
     return id
 
+#为api/disable_promotion.yml提供id
 def get_ID_of_promotion():
-    with open(ID_of_promotion_PATH, 'r') as f:
-        id: str = f.read()
+    #实际应该考虑若第一个不是禁用状态的促销该怎么办
+    if os.path.getsize(ID_of_promotion_PATH)!=0:
+        with open(ID_of_promotion_PATH, 'r') as f:
+            id: str = f.read()
     return id
 
 # #方式2：获取新建的状态未启用的单品促销ID_of_promotion
